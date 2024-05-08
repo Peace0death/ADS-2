e3e147868b64b549689f29e4b96cc2c6aaeb1284
Peace0death <160275048+Peace0death@users.noreply.github.com>
2024-05-08 20:20:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; i++) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t result = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+    double resr = 1.0;
+    for (uint64_t i = 1; i <= count; i++) {
+        resr += calcItem(x, i);
+    }
+    return resr;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double resd = 0;
+    for (uint64_t i = 1; i <= count; i++) {
+        resd += pown(-1, i - 1) * calcItem(x, (2 * i) - 1);
+    }
+    return resd;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double resd = 0;
+    for (uint16_t i = 1; i <= count; i++) {
+        resd += pown(-1, i - 1) * calcItem(x, (2 * i) - 2);
+    }
+    return resd;
 }
